9df6f8997e55e516ff7c8105270c49d5dee5cebf
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2024-09-19 19:35:03 +0200

    finishing data acquisition

--- data_acquisition.py
+++ data_acquisition.py
@@ -3,25 +3,31 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 from passwords_gitignore import get_token, get_org
 
-URL="10.45.98.1:8086"
-BUCKET = "wilga-prod"
-ORG = get_org()
-TOKEN = get_token()
+def acquire_data(url, bucket, org, token, time_in_minutes=10):
+    client = influxdb_client.InfluxDBClient(
+    url=url,
+    token=token,
+    org=org
+    )
 
-client = influxdb_client.InfluxDBClient(
-   url=URL,
-   token=TOKEN,
-   org=ORG
-)
+    query_api = client.query_api()
 
-query_api = client.query_api()
+    QUERY = f'from(bucket:"{bucket}") \
+        |> range(start: -{time_in_minutes}m) \
+        |> filter(fn:(r) => r._field == "value")'
 
-QUERY = f'from(bucket:"{BUCKET}") \
-    |> range(start: -10m) \
-    |> filter(fn:(r) => r._field == "value")'
+    data_acquired = query_api.query(query=QUERY, org=org)
 
-data_acquired = query_api.query(query=QUERY, org=ORG)
+    print(data_acquired)
 
-for table in data_acquired:
-    for record in table.records:
-        print(record.values["entity_id"], record.get_measurement(), record.get_value())
+    for table in data_acquired:
+        for record in table.records:
+            print(record.get_time(), record.values["entity_id"], record.get_value(), record.get_measurement())
+
+if __name__ == "__main__":
+    URL="10.45.98.1:8086"
+    BUCKET = "wilga-prod"
+    ORG = get_org()
+    TOKEN = get_token()
+
+    acquire_data(URL, BUCKET, ORG, TOKEN, 10)

8eb5c7da351644b38e28ac12ed5d93cbce2029bd
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2024-09-19 19:41:58 +0200

    collecting the data and naming it

--- data_acquisition.py
+++ data_acquisition.py
@@ -3,6 +3,13 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 from passwords_gitignore import get_token, get_org
 
+class AcquiredData:
+    def __init__(self, time, entity, value, unit):
+        self.time = time
+        self.entity = entity
+        self.value = value
+        self.unit = unit
+
 def acquire_data(url, bucket, org, token, time_in_minutes=10):
     client = influxdb_client.InfluxDBClient(
     url=url,
@@ -18,11 +25,13 @@ def acquire_data(url, bucket, org, token, time_in_minutes=10):
 
     data_acquired = query_api.query(query=QUERY, org=org)
 
-    print(data_acquired)
+    data = []
 
     for table in data_acquired:
         for record in table.records:
-            print(record.get_time(), record.values["entity_id"], record.get_value(), record.get_measurement())
+            data.append(AcquiredData(record.get_time(), record.values["entity_id"], record.get_value(), record.get_measurement()))
+
+    return data
 
 if __name__ == "__main__":
     URL="10.45.98.1:8086"
@@ -30,4 +39,4 @@ if __name__ == "__main__":
     ORG = get_org()
     TOKEN = get_token()
 
-    acquire_data(URL, BUCKET, ORG, TOKEN, 10)
+    acquired_data = acquire_data(URL, BUCKET, ORG, TOKEN, 10)

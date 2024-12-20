3a02c1a37998bf4a8f9b04ff4fa133e7127157b6
Roszczyk <mikolaj.roszczyk@onet.pl>
2024-12-20 21:47:39 +0100

    code refactor

--- AI_Testing_Edge/decision_tree/build_tree.py
+++ AI_Testing_Edge/decision_tree/build_tree.py
@@ -2,14 +2,14 @@ import sys
 import os 
 from pathlib import Path
 
-from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-def initFile(fileName):
-    path_to_data = Path(__file__).parent.parent.parent / fileName
-    file=open(path_to_data)
+
+def initFile(file_path):
+    file=open(file_path)
     dataArray=[]
     file_text=file.read()
     file_lines=file_text.split("\n")
@@ -22,6 +22,7 @@ def initFile(fileName):
     dataArray.pop(0)
     return dataArray
 
+
 def strings_to_ints(data):
     translator = dict({
         "wypocz" : 0,
@@ -33,29 +34,8 @@ def strings_to_ints(data):
         data[i][0] = translator[data[i][0]]
     return data
 
-data = initFile("ML_visit_purpose/prepared_data.csv")
-data = strings_to_ints(data)
 
-Xs = []
-Ys = []
-for d in data:
-    Y = d.pop(0)
-    Ys.append(Y)
-    Xs.append(d)
-
-X_train, X_test, y_train, y_test = train_test_split(Xs, Ys, random_state=1)
-
-tree = DecisionTreeClassifier(random_state=42)
-tree.fit(X_train, y_train)
-
-pred = tree.predict(X_test)
-accuracy = accuracy_score(y_test, pred)
-
-tree_capture = tree.tree_
-
-DEPTH = tree_capture.max_depth
-
-def export_tree(tree, feature_names=None):
+def export_tree(tree):
     tree_structure = []
     index = [[0] * 2**(DEPTH - 1) for _ in range(DEPTH)]
     condition = [[0] * 2**(DEPTH - 1) for _ in range(DEPTH)]
@@ -85,8 +65,6 @@ def export_tree(tree, feature_names=None):
     recurse(0)
     return tree_structure, index, condition, results
 
-tree_data, index_array, condition_array, results_array = export_tree(tree_capture)
-
 def tree_to_c(index, condition, results):
     all_index_string = "{\n"
     for row in index:
@@ -97,7 +75,6 @@ def tree_to_c(index, condition, results):
         all_index_string = all_index_string + f"{row_string},\n"
     all_index_string = all_index_string.rstrip(",") + "\n}\n"
 
-    
     all_condition_string = "{\n"
     for row in condition:
         row_string = "{"
@@ -114,23 +91,52 @@ def tree_to_c(index, condition, results):
 
     return all_index_string, all_condition_string, all_results_string
 
-print(len(results_array))
-index_array, condition_array, results_array = tree_to_c(index_array, condition_array, results_array)
 
+def visualize_tree(tree):
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(12, 8))  # Rozmiar wykresu
+    plot_tree(tree)
+    plt.show()
 
-print("INDEX:\n", index_array)
-print("CONDITIONS:\n", condition_array)
-print("RESULTS:\n", results_array)
-print("DEPTH: ", DEPTH)
+if __name__ == "__main__":
 
-# import matplotlib.pyplot as plt
-# plt.figure(figsize=(12, 8))  # Rozmiar wykresu
-# plot_tree(tree)
-# plt.show()
+    path_to_data = Path(__file__).parent.parent.parent / "ML_visit_purpose/prepared_data.csv"
+    data = initFile(path_to_data)
+    data = strings_to_ints(data)
 
-single_vector = [
-    4.0, 8.0, 28.669395973154366, 28.226168574812476, 27.78294117647059, 54.27523489932888, 
-    54.1679115673115, 54.06058823529412, 0.005036419911470156, 0.034154420236290284, 0.03665182812583781
-]
-prediction = tree.predict([single_vector])
-print("Prediction for single vector:", prediction)
+    Xs = []
+    Ys = []
+
+    for d in data:
+        Y = d.pop(0)
+        Ys.append(Y)
+        Xs.append(d)
+
+    X_train, X_test, y_train, y_test = train_test_split(Xs, Ys, random_state=1)
+
+    tree = DecisionTreeClassifier(random_state=42)
+    tree.fit(X_train, y_train)
+
+    pred = tree.predict(X_test)
+    accuracy = accuracy_score(y_test, pred)
+
+    tree_capture = tree.tree_
+
+    DEPTH = tree_capture.max_depth
+
+    tree_data, index_array, condition_array, results_array = export_tree(tree_capture)
+    index_array, condition_array, results_array = tree_to_c(index_array, condition_array, results_array)
+
+    print("INDEX:\n", index_array)
+    print("CONDITIONS:\n", condition_array)
+    print("RESULTS:\n", results_array)
+    print("DEPTH: ", DEPTH)
+
+    single_vector = [
+        4.0, 8.0, 28.669395973154366, 28.226168574812476, 27.78294117647059, 54.27523489932888, 
+        54.1679115673115, 54.06058823529412, 0.005036419911470156, 0.034154420236290284, 0.03665182812583781
+    ]
+    prediction = tree.predict([single_vector])
+    print("Prediction for single vector:", prediction)
+
+    # visualize_tree(tree)

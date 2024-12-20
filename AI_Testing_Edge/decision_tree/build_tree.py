613d18ea1540e77d939d03c6f95d28c074f5f565
Roszczyk <mikolaj.roszczyk@onet.pl>
2024-12-20 15:32:11 +0100

    working on results, rowNumber*2**(DEPTH-level)

--- AI_Testing_Edge/decision_tree/build_tree.py
+++ AI_Testing_Edge/decision_tree/build_tree.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import numpy as np
 
 def initFile(fileName): #odczytanie danych z pliku i stworzenie obiektów danych
     path_to_data = Path(__file__).parent.parent.parent / fileName
@@ -50,21 +51,16 @@ tree.fit(X_train, y_train)
 pred = tree.predict(X_test)
 accuracy = accuracy_score(y_test, pred)
 
-print(pred)
-print(y_test)
-print(accuracy)
-
 tree_capture = tree.tree_
 
 DEPTH = tree_capture.max_depth
-print(DEPTH)
 
 def export_tree(tree, feature_names=None):
     tree_structure = []
     index = [[0] * 2**(DEPTH-1) for _ in range(DEPTH)]
     condition = [[0] * 2**(DEPTH-1) for _ in range(DEPTH)]
-    print("size:", len(index), len(index[0]))
-    def recurse(node, level=0, rowNumber = 0):
+    results = [0] * 2**DEPTH
+    def recurse(node, level = 0, rowNumber = 0):
         if tree.feature[node] != -2:
             tree_structure.append({
                 "level": level,
@@ -73,7 +69,6 @@ def export_tree(tree, feature_names=None):
                 "left": tree.children_left[node],
                 "right": tree.children_right[node],
             })
-            print(level, rowNumber)
             index[level][rowNumber] = int(tree.feature[node])
             condition[level][rowNumber] =  float(tree.threshold[node])
             recurse(tree.children_left[node], level + 1, rowNumber = rowNumber * 2)
@@ -83,26 +78,47 @@ def export_tree(tree, feature_names=None):
                 "level": level,
                 "result": float(tree.value[node][0][0])
             })
+            for i in range(rowNumber * 2**(DEPTH-level), (rowNumber + 1) * 2**(DEPTH-level)):
+                print(tree.value[node][0][0])
+                results[i] = tree.value[node][0][0]
+            
     recurse(0)
-    return tree_structure
+    return tree_structure, index, condition, results
 
-tree_data = export_tree(tree_capture)
+tree_data, index_array, condition_array, results_array = export_tree(tree_capture)
 
-print(tree_data)
+def tree_to_c(index, condition, results):
+    all_index_string = "{"
+    for row in index:
+        row_string = "{"
+        for i in row:
+            row_string = row_string + f"{i},"
+        row_string = row_string.rstrip(",") + "}"
+        all_index_string = all_index_string + f"{row_string},"
+    all_index_string = all_index_string.rstrip(",") + "}"
 
-# def tree_to_c(tree_data):
-#     c_code = []
-#     for i, node in enumerate(tree_data):
-#         if "result" in node:  # Liść
-#             c_code.append(f"{{.level={node['level']}, .result={node['result']}}}")
-#         else:  # Węzeł wewnętrzny
-#             c_code.append(
-#                 f"{{.level={node['level']}, .index={node['index']}, .threshold={node['threshold']}, "
-#                 f".below={node['left']}, .over={node['right']}}}"
-#             )
-#     return "TreeNode nodes[] = {" + ",\n".join(c_code) + "};"
+    
+    all_condition_string = "{"
+    for row in condition:
+        row_string = "{"
+        for i in row:
+            row_string = row_string + f"{i},"
+        row_string = row_string.rstrip(",") + "}"
+        all_condition_string = all_condition_string + f"{row_string},"
+    all_condition_string = all_condition_string.rstrip(",") + "}"
 
-# c_tree_code = tree_to_c(tree_data)
-# print(c_tree_code)
-# print(tree_data)
+    all_results_string = "{"
+    for i in results:
+        all_results_string = all_results_string + f"{int(i)},"
+    all_results_string = all_results_string.rstrip(",") + "}"
 
+    return all_index_string, all_condition_string, all_results_string
+
+print(len(results_array))
+index_array, condition_array, results_array = tree_to_c(index_array, condition_array, results_array)
+
+
+print("INDEX:\n", index_array)
+print("CONDITIONS:\n", condition_array)
+print("RESULTS:\n", results_array)
+print("DEPTH: ", DEPTH)

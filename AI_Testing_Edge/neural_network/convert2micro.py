752ee303364c4ac5048d1ce24d0c0714aa290f97
Roszczyk <mikolaj.roszczyk@onet.pl>
2024-12-29 15:13:20 +0100

    refactor

--- AI_Testing_Edge/neural_network/convert2micro.py
+++ AI_Testing_Edge/neural_network/convert2micro.py
@@ -5,10 +5,8 @@ from pathlib import Path
 original_model = keras.models.load_model(Path(__file__).parent / "model.keras")
 
 converter = tf.lite.TFLiteConverter.from_keras_model(original_model)
-# Kwantyzacja (opcjonalna, dla mniejszych modeli)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
 
-# Zapisanie modelu jako .tflite
 with open(Path(__file__).parent / "model.tflite", "wb") as f:
     f.write(tflite_model)
